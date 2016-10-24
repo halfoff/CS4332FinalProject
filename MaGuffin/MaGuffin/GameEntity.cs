1d33ca90a634d92cb4408273610e5c4b1c6a8adb
halfoff <psychotuna@gmail.com>
2016-10-24 10:45:27 -0500

    Added GameEntity Class

---
+++ MaGuffin/MaGuffin/GameEntity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaGuffin
+{
+    
+    class GameEntity
+    {
+        //Position of the GE
+        private Vector2 loc;
+        //Graphic representation of GE
+        Texture2D sprite;
+        //Size of GameEntity in px
+        private int height, width;
+
+        public GameEntity()
+        {
+            loc = new Vector2(0, 0);
+        }
+    }
+}
